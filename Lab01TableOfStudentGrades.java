54c9278e4bc7e241de447cb1c7d90eba613cdf92
shrmur <67557087+shrmur@users.noreply.github.com>
2021-04-24 11:15:08 -0400

    Add files via upload

---
+++ Lab01TableOfStudentGrades.java
@@ -0,0 +1,23 @@
+//name:Shrish Murugapandi date:9/9/20
+//Lab01TableOfStudentGrades
+public class Lab01TableOfStudentGrades
+{
+   public static void main(String[] args)
+   
+   {
+      System.out.println ("/// /// /// /// ///"+ "\\\\\\ \\\\\\ \\\\\\ \\\\\\ \\\\\\ ");
+      System.out.println("=="+'\t'+'\t'+'\t'+'\t'+"Student Points"+'\t'+'\t'+'\t'+'\t'+"==");
+      System.out.println ("/// /// /// /// ///"+ "\\\\\\ \\\\\\ \\\\\\ \\\\\\ \\\\\\ ");
+      System.out.println("Name"+'\t'+'\t'+'\t'+'\t'+"Lab"+'\t'+'\t'+"Bonus"+'\t'+"\t"+"Total");
+      System.out.println("----"+'\t'+'\t'+'\t'+'\t'+"---"+'\t'+'\t'+"-----"+'\t'+"\t"+"-----");
+      System.out.println("Shrish"+'\t'+'\t'+'\t'+"44"+'\t'+'\t'+'\t'+"7"+'\t'+"\t"+'\t'+(44+7));
+      System.out.println("Chran"+'\t'+'\t'+'\t'+'\t'+"59"+'\t'+'\t'+'\t'+"11"+'\t'+"\t"+'\t'+(59+11));
+      System.out.println("Mo"+'\t'+'\t'+'\t'+'\t'+'\t'+"29"+'\t'+'\t'+'\t'+"10"+'\t'+"\t"+'\t'+(29+10));
+      System.out.println("Assad"+'\t'+'\t'+'\t'+'\t'+"40"+'\t'+'\t'+'\t'+"9"+'\t'+"\t"+'\t'+(40+9));
+      System.out.println("Farhan"+'\t'+'\t'+'\t'+"51"+'\t'+'\t'+'\t'+"17"+'\t'+"\t"+'\t'+(51+17));
+
+      
+
+   }
+
+}
